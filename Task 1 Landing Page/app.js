8f95ff958405689f000030da823764ba9633bcee
Balla Kavya <ballakavya594@gmail.com>
2024-05-16 18:40:41 +0530

    Update

--- Task 1 Landing Page/app.js
+++ Task 1 Landing Page/app.js
@@ -10,3 +10,11 @@ navLinks.forEach(link => {
     });
   });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.querySelector('.toggle-button');
+  const navList = document.querySelector('nav ul');
+
+  toggleButton.addEventListener('click', function () {
+    navList.classList.toggle('active');
+  });
+});
